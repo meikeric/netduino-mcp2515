d85e09030ea79bb4a08e938541a9b1f7785fc8f7
dan.velago <dan.velago@gmail.com>
2011-10-04 23:34:30 +0000

    Initial upload. Still on development. TX works for standard and extended messages. RX works only for standard 11 bit frames.

---
+++ Code/CANTestBed/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.NetduinoPlus;
+
+namespace CANTestBed
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int cnt = 0;
+            bool bootState = false;
+            bool txLedState = false;
+            
+            OutputPort txled = new OutputPort(Pins.GPIO_PIN_D8, txLedState);
+            OutputPort bootLed = new OutputPort(Pins.ONBOARD_LED, bootState);
+
+            // Blink the netduino led to indicate boot state.
+            for (int i = 0; i < 3; i++)
+            {
+                bootLed.Write(bootState);
+                Thread.Sleep(500);
+                bootState = !bootState;
+            }
+
+            // Create the CAN Handler.
+            MCP2515 CANHandler = new MCP2515();
+            CANHandler.InitCAN(MCP2515.enBaudRate.CAN_BAUD_500K);
+            // Set to normal operation mode.
+            CANHandler.SetCANNormalMode();
+
+            // Create standard TX message.
+            MCP2515.CANMSG txMessage = new MCP2515.CANMSG();
+            txMessage.data = new byte[] { 0xCC, 0xAA, 0xAA, 0xAA, 0x11, 0x00, 0xFF, 0xFF };
+            txMessage.CANID = 0x1AA;
+
+            // Create extended TX message.
+            MCP2515.CANMSG txMessageExt = new MCP2515.CANMSG();
+            txMessageExt.CANID = 0x1FEDCBA0;
+            txMessageExt.data = new byte[] { 0x00, 0xEE };
+
+            // Create the message that will hold received messages.
+            MCP2515.CANMSG rxMessage = new MCP2515.CANMSG();
+
+            while (true)
+            {
+                // Put a counter in the message.
+                txMessage.data[6] = (byte)(cnt>>8);
+                txMessage.data[7] = (byte)cnt;
+                // Transmit messages.
+                CANHandler.Transmit(txMessage, 10);
+                CANHandler.Transmit(txMessageExt, 10);
+                // Blink CAN shield TX led every 5 cycles.
+                txLedState = (cnt % 5 == 0) ? !txLedState : txLedState;
+                txled.Write(txLedState);
+                // Check if a message was received.
+                if (CANHandler.Receive(out rxMessage, 20))
+                {
+
+                }
+                // Increase counter.
+                cnt++;
+            }
+        }
+
+    }
+}
---
+++ Code/CANTestBed/Properties/AssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MCP2515 Handler")]
+[assembly: AssemblyDescription("MCP2515 library and example to control CAN bus.")]
+[assembly: AssemblyCopyright("Apache License, Version 2.0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.40.0.0")]
+[assembly: AssemblyFileVersion("0.40.0.0")]
+/*****************************************************************************
+ * Date         Version      Description
+****************************************************************************** 
+ * 2011-10-04   BETA 0.40    Initial release
+ * 
+ * 
+ * 
+ *****************************************************************************/
